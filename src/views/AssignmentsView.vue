6ffaafa3b379c633f25a26adebdf783068cc1adc
zqleow13 <hayleyleow13@gmail.com>
2023-10-11 14:28:48 +0800

    Styled the assignments elements

--- src/views/AssignmentsView.vue
+++ src/views/AssignmentsView.vue
@@ -7,8 +7,29 @@
         <h1>Assignments View</h1>
         <div class="assignments">
             <a v-for="assignment in assignments" :key="assignment.id" href="">
-                {{ assignment.subject }}
+                {{ assignment.ref }}
             </a>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .assignments {
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        gap: 20px;
+    }
+
+    .assignments a {
+        color: black;
+        width: 100%;
+        box-shadow: 1px 1px 1px black;
+        border-radius: 5px;
+        background-color: aliceblue;
+        padding: 1rem;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        
+    }
+
+</style>
