ba09ee161d1b825f3c70bf23afa379565e5721c7
zqleow13 <hayleyleow13@gmail.com>
2023-10-15 21:24:07 +0800

    redid my assignment's contents

--- src/views/AssignmentContentView.vue
+++ src/views/AssignmentContentView.vue
@@ -1,9 +1,18 @@
 <script setup>
+    import {useRoute} from "vue-router"
+    import assignments from "../data/assignments.json"
+    const route = useRoute()
+
+    const assignment = assignments.find(a => a.id === parseInt(route.params.id))
+    
 </script>
 
 <template>
     <div>
         <h1>Assignment Content View</h1>
-        <p>you</p>
+        <p>{{assignment.ref}}</p>
+        <p>{{assignment.subject}}</p>
+        <p>{{assignment.location}}</p>
+        <p>{{assignment.rates}}</p>
     </div>
 </template>
