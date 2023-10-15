4468c53c9c351411b071278b2c4fe50495aecb69
zqleow13 <hayleyleow13@gmail.com>
2023-10-15 22:24:38 +0800

    Adding a search bar to filter out assignments

--- src/views/AssignmentsView.vue
+++ src/views/AssignmentsView.vue
@@ -1,13 +1,9 @@
-<script setup>
-    import { RouterLink } from "vue-router"
-    import assignments from "../data/assignments.json"
-</script>
-
 <template>
     <div>
         <h1>Assignments View</h1>
         <div class="assignments">
-            <RouterLink :to="`/findassignments/assignments/${assignment.id}`" v-for="assignment in assignments" :key="assignment.id" href="">
+        <input type="text" placeholder="Search for assignments" v-model="search">
+            <RouterLink :to="`/findassignments/assignments/${assignment.id}`" v-for="assignment in searchedAssignments" :key="assignment.id" href="">
                 {{ assignment.ref }}
                 {{ assignment.subject }}
                 {{ assignment.location }}
@@ -15,6 +11,21 @@
         </div>
     </div>
 </template>
+
+<script setup>
+    import { RouterLink } from "vue-router"
+    import assignments from "../data/assignments.json"
+    import { computed } from "vue"
+    import { ref } from "vue"
+
+    const search = ref('')
+
+    const searchedAssignments = computed(() => {
+        return assignments.filter((assignment) => {
+            return assignment.subject.match(search)
+        })
+    })
+</script>
 
 <style scoped>
     .assignments {
